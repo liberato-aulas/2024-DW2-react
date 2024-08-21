dab55745ea409af1ac7b16b25e9b09234b961a56
Rafael <liberato@utfpr.edu.br>
2024-08-21 11:00:06 -0300

    projeto em branco

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+export function App(){
+
+  return (
+    <>
+      <h1>Título</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque quidem dicta odio esse quasi, quae corrupti enim, magnam delectus adipisci corporis, vitae quibusdam a incidunt nobis laboriosam praesentium nam id!</p>
+    </>
+  )
+}
+
