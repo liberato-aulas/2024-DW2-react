aa1bbfc444106e402ac2f8d13f7f6fa51940704e
Rafael <liberato@utfpr.edu.br>
2024-08-21 12:05:44 -0300

    Aula 21/08/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
 
 
 export function App(){
 
   return (
     <>
-      <h1>Título</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque quidem dicta odio esse quasi, quae corrupti enim, magnam delectus adipisci corporis, vitae quibusdam a incidunt nobis laboriosam praesentium nam id!</p>
+      <Header/>
+      <Main/>
+      <Footer/>
+
     </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,6 @@
+export function Main() {
+
+    return (
+        <h1>Main</h1>
+    )
+}
